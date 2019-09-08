4d7f845cf085bf33bc779addf842a0a538c84f2f
Chester Wood <chetstone@malamode.io>
2019-09-08 15:28:13 -0600

    seem to have broken something. ?

--- android/src/main/java/com/dooboolab/RNIap/RNIapModule.java
+++ android/src/main/java/com/dooboolab/RNIap/RNIapModule.java
@@ -289,8 +289,9 @@ public class RNIapModule extends ReactContextBaseJavaModule implements Purchases
           item.putString("developerPayloadAndroid", purchase.getDeveloperPayload());
           item.putString("signatureAndroid", purchase.getSignature());
           item.putInt("purchaseStateAndroid", purchase.getPurchaseState());
-          item.putBoolean("isAcknowledged", purchase.isAcknowledged());
-          
+          if (type.equals(BillingClient.SkuType.INAPP)) {
+            item.putBoolean("isAcknowledged", purchase.isAcknowledged());
+          }
           if (type.equals(BillingClient.SkuType.SUBS)) {
             item.putBoolean("autoRenewingAndroid", purchase.isAutoRenewing());
           }
